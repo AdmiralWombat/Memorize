377b0339ebbc92c775d3bf57b43d3213f3f6514b
David Hardzinski <dmhardzi@mtu.edu>
2025-04-18 21:13:09 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Wombat on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
